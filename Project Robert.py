ab72581ca3d9abf1312aa39da9adf96263167389
KESSMARX <jlkessemeier@gmail.com>
2025-10-17 16:48:34 +0200

    Case 2 start set up

--- Project Robert.py
+++ Project Robert.py
@@ -16,42 +16,78 @@ print("*(1) Load a data set.                                    *")
 print("*(2) Train the data set.                                 *")
 print("*(3) Evaluation of the data set.                         *")
 print("*(4) Simulation of the data set.                         *")
+print("**********************************************************")
 
 # Variables set up
+userChoice = 0
 menuEdit = 1
+successLoad = 0
+
+# Function set up
+def loadData():
+    url = input("Insert the name for the dataset: ")
+    print("The dataset was loaded sucessfully.")
+    global successLoad # calling the global variable 
+    successLoad = 1    # telling system the load was successfull
+    df = pd.read_csv(url)
+    print(df.head(10))
+    print(df.info())
+    print(df.describe())
+    print(df.shape)
 
 # While loop
 while menuEdit == 1:
     
-    # Set up of variables
     try:
-        userChoice = int(input("Please type in the number of the choosen operation: "))
+        userChoice = int(input("\nPlease type in the number of the choosen operation: "))
     
     except:
-        print ("ERROR - wrong input!")
+        print ("\nERROR - wrong input!")
         userChoice = int(input("Please type in a number: "))
 
     match userChoice:
         case 1:
             # ---------------------------------------------------------------------------------------------
             # 1. LOADING
-            def loadData():
-                url = input("Insert the name for the dataset: ")
-                print("The dataset was loaded sucessfully.")
-                df = pd.read_csv(url)
-                print(df.head(10))
-                print(df.info())
-                print(df.describe())
-                print(df.shape)
             loadData()
 
-            menuEdit = int(input("(1) continue or (2) cancel "))
+            menuEdit = int(input("\n(1) continue or (2) cancel "))
 
 
         case 2:
             # ---------------------------------------------------------------------------------------------
             # 2. TRAINING 
-            pass
+            if successLoad == 1:
+                print("**********************************************************")
+                print("******************* Training Algorithms ******************")
+                print("*                                                        *")
+                print("* Please choose one of the following options:            *")
+                print("*                                                        *")
+                print("*(1) Split the data into train sets.                     *")
+                print("*(2) Split the data into stratified train sets.          *")
+                print("**********************************************************")
+
+                userChoice = int(input("\nPlease type in the number of the choosen algorithm: "))
+
+                match userChoice:
+                    case 1:
+                        # Split the data into train/test sets
+                        features_train, features_test, classes_train, classes_test = train_test_split(
+                        features, classes, test_size=0.2, random_state=10
+                        )
+
+                    case 2:
+                        # Split the data into STRATIFIED train/test sets:
+                        strat_feat_train, strat_feat_test, strat_classes_train, strat_classes_test = train_test_split(
+                        features, classes, test_size=0.4, random_state=10, stratify=classes
+                        )
+
+
+            else:
+                loadData()
+
+
+            menuEdit = int(input("\n(1) continue or (2) cancel "))
 
         case 3:
             # ---------------------------------------------------------------------------------------------
@@ -67,6 +103,11 @@ while menuEdit == 1:
 
         case _:
             print("Invalid input!")
-            menuEdit = int(input("(1) continue or (2) cancel "))
+            menuEdit = int(input("\n(1) continue or (2) cancel "))
 
+
+    
 # ---------------------------------------------------------------------------------------------
+# Code ending
+if menuEdit == 2:
+    print ("\nThanks for your time and have a great day!\n")
