0664c6e1fbd1015803e770ea6b46ba23ff8229ef
KESSMARX <jlkessemeier@gmail.com>
2025-10-20 14:10:38 +0200

    Loop Fix

--- Project Robert.py
+++ Project Robert.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -22,39 +23,45 @@ print("**********************************************************")
 userChoice = 0
 menuEdit = 1
 successLoad = 0  
-features = ""
-classes = "" 
-userattempt = 1    
+knn = KNeighborsClassifier(n_neighbors=1)     # Creates a Knn Classifier Object with k=1
+predictions = 0
+features = 0
+classes = 0
+userattempt = 1
 
 # Function set up
 def loadData():
-    url = input("Insert the name for the dataset: ")
-    print("The dataset was loaded sucessfully.")
-    global successLoad # calling the global variable 
-    successLoad = 1    # telling system the load was successfull
-    df = pd.read_csv(url)
-    print(df.head(10))
-    print(df.info())
-    print(df.describe())
-    print(df.shape)
+    global userattempt
+    while userattempt == 1:
+        try:
+            url = input("Insert the name for the dataset: ")
+            global successLoad # calling the global variable 
+            successLoad = 1    # telling system the load was successfull
+            df = pd.read_csv(url)
+            print("The dataset was loaded sucessfully.")
+            print(df.head(10))
+            print(df.info())
+            print(df.describe())
+            print(df.shape)
+            # Creates a dataframe using the drop method, which has two parameters:
+            #   The first parameter tells which labels to remove (Columns Name) or 
+            #   The second parameter tells whether to remove a row index or	
+            #   a column name. axis=1 means we want to remove a column.
+            global features # calling the global variable
+            features = df.drop("class",axis=1)
 
-    # Creates a dataframe using the drop method, which has two parameters:
-    #   The first parameter tells which labels to remove (Columns Name) or 
-    #   The second parameter tells whether to remove a row index or	
-    #   a column name. axis=1 means we want to remove a column.
-    global features # calling the global variable
-    features = df.drop("class",axis=1)
+            # Creates a dataframe from just one column:
+            global classes # calling the global variable
+            classes = df["class"]
+            break
+        except: 
+            print ("ERROR")
+            userattempt = int(input("\n(1) continue or (2) cancel "))
+    
+def end():
+    print ("\nThanks for your time and have a great day!\n")
+    sys.exit()
 
-    # Creates a dataframe from just one column:
-    global classes # calling the global variable
-    classes = df["class"]
-
-def train():
-    # Creates a Knn Classifier Object with k=1
-    knn = KNeighborsClassifier(n_neighbors=1)
-
-    # Trains this Knn Classifier with the training set obtained previously:
-    knn.fit(strat_feat_train, strat_classes_train)
 
 
 # While loop
@@ -62,7 +69,7 @@ while menuEdit == 1:
     while userattempt == 1:
         try:
             userChoice = int(input("\nPlease type in the number of the choosen operation: "))
-            userattempt = 0        
+            break        
         except:
             print ("\nERROR - wrong input!")
         
@@ -101,7 +108,12 @@ while menuEdit == 1:
                         features_train, features_test, classes_train, classes_test = train_test_split(
                         features, classes, test_size=0.2, random_state=10
                         )
-#                        train()
+                 
+                        # Trains this Knn Classifier with the training set obtained previously:
+                        knn.fit(features_train, classes_train)
+
+                        predictions = knn.predict(features_test)
+
                         print ("You have trained this set of data!")
 
                     case 2:
@@ -109,7 +121,12 @@ while menuEdit == 1:
                         strat_feat_train, strat_feat_test, strat_classes_train, strat_classes_test = train_test_split(
                         features, classes, test_size=0.4, random_state=10, stratify=classes
                         )
-#                        train()
+                 
+                        # Trains this Knn Classifier with the training set obtained previously:
+                        knn.fit(strat_feat_train, strat_classes_train)
+
+                        predictions = knn.predict(strat_feat_test)
+                      
                         print ("You have trained this stratified set of data!")                     
 
 
@@ -138,7 +155,7 @@ while menuEdit == 1:
 # ---------------------------------------------------------------------------------------------
 # Code ending
 if menuEdit == 2:
-    print ("\nThanks for your time and have a great day!\n")
+    end()
 
 
 
